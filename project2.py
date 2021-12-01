672e1a71f105a5592683855f570c54f46d5c8bc2
shebosh <89801538+shebosh@users.noreply.github.com>
2021-11-30 21:51:07 -0600

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,25 @@
+import nltk
+
+# Ex. 18 Bigrams
+from nltk.corpus import brown
+from nltk.corpus import webtext
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+textWords = [w.lower() for w in webtext.words('pirates.txt')]
+finder = BigramCollocationFinder.from_words(textWords)
+finder.nbest(BigramAssocMeasures.likelihood_ratio, 10)
+
+# Ex 19
+# table of word frequencies by genre
+religion_text = brown.words(categories='religion')
+fdist = nltk.FreqDist(w.lower() for w in religion_text)
+commons = ['hell', 'heaven', 'Lord', 'church', 'sin', 'angels']
+for c in commons:
+    print(c + ':', fdist[c], end=' ')
+
+cfd = nltk.ConditionalFreqDist((genre, word)
+            for fileid in gutenberg.fileids()
+            for word in gutenberg.words(fileids)
+fileids=['shakespeare-caesar.txt', 'shakespeare-hamlet.txt']
+words=['tragedy', 'trouble', 'love', 'doubt']
+cfd.tabulate(conditions=fileids, samples=words)
